bd3778e374a0fd18b40d58611bb1e0d0405ad693
i_vvvy <ivvvy.e@gmail.com>
2022-05-29 20:21:53 +0900

    마이스터디 페이지 및 마이스터디 리스트 레이아웃

---
+++ src/components/myStudy/MyStudyList.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { style } from './MyStudyList.style';
+const StudyList = () => {
+  return <StudyListContainer />;
+};
+
+export default StudyList;
+
+const { StudyListContainer } = style;
--- src/pages/myStudyPage/MyStudyPage.component.jsx
+++ src/pages/myStudyPage/MyStudyPage.component.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import { style } from './MyStudyPage.style';
+
+import StudyList from '../../components/myStudy/MyStudyList.component';
 
 const MyStudyPage = () => {
-  return <div>MyStudyPage</div>;
+  const { TabPane } = Tabs;
+
+  const onChange = key => {
+    console.log(key);
+  };
+
+  return (
+    <MyStudyContainer>
+      <TopBanner>
+        <h2>마이 스터디</h2>
+        <span>ooLa와 함께하는 스터디</span>
+      </TopBanner>
+      <TabsContainer>
+        <Tabs defaultActiveKey="1" onChange={onChange}>
+          <TabPane tab="진행스터디" key="1">
+            <StudyList />
+          </TabPane>
+          <TabPane tab="내가만든스터디" key="2">
+            <StudyList />
+          </TabPane>
+          <TabPane tab="진행예정스터디" key="3">
+            <StudyList />
+          </TabPane>
+          <TabPane tab="완료스터디" key="4">
+            <StudyList />
+          </TabPane>
+          <TabPane tab="관심스터디" key="5">
+            <StudyList />
+          </TabPane>
+        </Tabs>
+      </TabsContainer>
+    </MyStudyContainer>
+  );
 };
 
 export default MyStudyPage;
+
+const { MyStudyContainer, TopBanner, TabsContainer } = style;
