fdcee8a03fc1423928d6aac12eac5c52ef36be6b
Trelawney <00uj@naver.com>
2022-05-30 14:55:36 +0900

    프로필 context 추가

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ProfileProvider } from './context/Profile.context';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProfileProvider>
+      <App />
+    </ProfileProvider>
   </React.StrictMode>,
 );
 
