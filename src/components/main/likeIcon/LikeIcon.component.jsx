a7f35924f1872aa1673f78e22f5ca1d4a4c027bd
Trelawney <00uj@naver.com>
2022-05-30 14:08:58 +0900

    진행스터디 필터링

--- src/components/main/likeIcon/LikeIcon.component.jsx
+++ src/components/main/likeIcon/LikeIcon.component.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { HeartFilled } from '@ant-design/icons';
+import PropTypes from 'prop-types';
 
-const LikeIcon = () => {
-  return <HeartFilled style={{ fontSize: '16px', color: '#787878' }} />;
+const LikeIcon = ({ isLike }) => {
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {isLike ? (
+        <HeartFilled style={{ fontSize: '16px', color: '#dc143c' }} />
+      ) : (
+        <HeartFilled style={{ fontSize: '16px', color: '#787878' }} />
+      )}
+    </>
+  );
+};
+
+LikeIcon.propTypes = {
+  isLike: PropTypes.bool,
 };
 
 export default LikeIcon;
