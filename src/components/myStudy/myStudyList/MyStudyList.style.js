59c702772315377cdb88be8c9105f2dfdb1de6ac
i_vvvy <ivvvy.e@gmail.com>
2022-05-31 10:55:31 +0900

    feat: 마이스터디페이지 레이아웃

---
+++ src/components/myStudy/myStudyList/MyStudyList.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StudyListContainer = styled.div`
+  display: flex;
+  min-width: 1100px;
+  margin-top: 30px;
+`;
+
+export const style = { StudyListContainer };
