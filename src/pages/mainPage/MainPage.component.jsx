4ebc4b968f32c96a90891b4839fc63717d1862f8
Trelawney <00uj@naver.com>
2022-06-19 19:01:06 +0900

    메인페이지 스터디리스트 GET요청 수정

--- src/pages/mainPage/MainPage.component.jsx
+++ src/pages/mainPage/MainPage.component.jsx
@@ -1,8 +1,16 @@
-import { Fragment } from 'react';
+import { Fragment, useContext, useEffect } from 'react';
+import { StudyListContext } from '../../context/StudyList.context';
 import Filtering from '../../components/main/filtering/Filtering.component';
 import MainStudyList from '../../components/main/mainStudyList/MainStudyList.component';
 
 const MainPage = () => {
+  const { setIsFilteringStart, studies } = useContext(StudyListContext);
+  useEffect(() => {
+    if (studies.length > 0) {
+      setIsFilteringStart(true);
+    }
+  }, []);
+
   return (
     <Fragment>
       <Filtering />
